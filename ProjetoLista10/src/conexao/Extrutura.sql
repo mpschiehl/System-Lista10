df14b66371e81e7625df945aa164b28078214c1d
Marcio <mpschiehl>
2018-07-13 11:13:53 -0300

    Desenvolvimento

---
+++ ProjetoLista10/src/conexao/Extrutura.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS lista10;
+CREATE DATABASE IF NOT EXISTS lista10;
+
+USE lista10;
+
+CREATE TABLE alunos(
+        id              INT AUTO_INCREMENT PRIMARY KEY,
+        nome            VARCHAR(100),
+        codigo_de_matricula VARCHAR(100),
+        nota_1          FLOAT,
+        nota_2          FLOAT,
+        nota_3          FLOAT,
+        nota_4          FLOAT,
+        frequencia      TINYINT);
---
+++ ProjetoLista10/src/interfaces/BaseInterfaceLista10.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+/**
+ *
+ * @author Marcio Pedro Schiehl
+ */
+public interface BaseInterfaceLista10 {
+     void gerarTela();
+    
+    void adicionarComponentes();
+    
+    void gerarLocalizacoes();
+    
+    void gerarDimensoes();
+    
+}
